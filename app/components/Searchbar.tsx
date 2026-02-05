c78c05a4dcdf1dd0eae1fa343b475607078c1d79
marymetilda <metildaphy@gmail.com>
2026-02-05 22:31:21 +0530

    feat: searchbar

---
+++ app/components/Searchbar.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { SubmitEventHandler } from "react";
+
+type SearchbarProps = {
+  onSearch: (value: string) => void;
+};
+
+export default function SearchBar({ onSearch }: SearchbarProps) {
+  const handleSubmit: SubmitEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    const value = (e.currentTarget.search.value || "").trim();
+    if (value) onSearch(value);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="flex gap-2">
+      <input
+        name="search"
+        placeholder="Search GIFs..."
+        className="border p-2 flex-1 rounded"
+      />
+      <button className="bg-black text-white px-4 rounded">Search</button>
+    </form>
+  );
+}
